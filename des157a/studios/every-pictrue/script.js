d3a75ee3b1a166f5085a6ef6fb3c84299c69dd88
Das Liao <03_acuity_eyeful@icloud.com>
2021-11-09 20:16:58 -0800

    update every picture

--- des157a/studios/every-pictrue/script.js
+++ des157a/studios/every-pictrue/script.js
@@ -8,46 +8,80 @@
     const theCurbs = document.querySelectorAll('.st0');
     const muTopImg = document.querySelector('#mu-top');
     const muTrace = document.querySelector('#mu-trace');
-    const theStart = document.querySelector('.start');
     const theBkg = document.querySelector('#bkg');
     const leftArrow = document.querySelector('#left-arrow');
+    const leftArrowPath = document.querySelector('#left-arrow .cls-1');
     const muContainer = document.querySelector('#mu-container');
+    const footer = document.querySelector('footer');
+    const bkgText = document.querySelector('#bkg div');
+    const header = document.querySelector('header');
+    const headerText = document.querySelectorAll("header h1");
+
+    
     
 
 
-    console.log(theBuses);
-    console.log(muTopImg);
-    console.log(muTrace);
-    console.log(theStart);
+    // console.log(theBuses);
+    // console.log(muTopImg);
+    // console.log(muTrace);
+    // console.log(theStart);
+    
 
     window.onbeforeunload = function () {
 		window.scrollTo(0, 0);
 	}
 
     window.addEventListener('load', function() {
-        const captions = document.querySelectorAll('section')
-        console.log(captions);
+        
+        // await loadImage("images/mu-day.jpg", muTopImg);
+
+        const captions = document.querySelectorAll('article section');
+        // console.log(captions);
         let secTops = [];
         let pageTop;
         let cnt = 1;
         let prevCnt = 1;
         let doneResizing;
-		let exitDir;
-		let enterDir;
+        let exitDir;
+        let enterDir;
+        
 
         const preLoader = document.querySelector('.mu-on-start');
         container.className = 'main-container-layout-after-load';
         preLoader.className = 'mu-after-load';
+        footer.className = 'footer-after-load';
+
+        const muImgAfterLoad = document.querySelector('.main-container-layout-after-load .mu-after-load .imgs');
+        console.log(muImgAfterLoad);
+
+        // if (window.innerHeight <= window.innerWidth) {
+        //     muImgAfterLoad.style.width = '100vw';
+        //     muImgAfterLoad.style.removeProperty('height');
+        // }
         
-        console.log(`cnt1 ${cnt}`);
         resetPage();
-        console.log(`cnt2 ${cnt}`);
 
 
         console.log(`left arrow ${leftArrow}`);
 
+        leftArrow.addEventListener('mouseover', function(){
+            leftArrowPath.style.fill = 'white';
+        });
+
+        leftArrow.addEventListener('mouseout', function(){
+            leftArrowPath.style.fill = '#eeeeefc2';
+        });
+
         leftArrow.addEventListener('click', function(){
             container.className = 'main-container-layout-before-scrolling';
+            header.style.top = '3vh';
+            header.style.left = "4vw";
+            headerText.forEach(function(text){
+                text.style.fontSize = '5vh';
+                text.style.lineHeight = "7vh";
+            });
+            headerText[0].style.color = 'rgb(177, 100, 100)';
+            footer.removeAttribute('class');
         });
 
         window.addEventListener('scroll', function(){
@@ -58,7 +92,12 @@
                     muTopImg.setAttribute('src', 'images/mu-night.jpg');
                 } else if (cnt == 5) {
                     muTopImg.setAttribute('src', 'images/mu-bw.jpg');
-                } else if (cnt == 1 || cnt == 4 || cnt == 6) {
+                } else if (cnt == 1) {
+                    muTopImg.setAttribute('src', 'images/mu-day.jpg');
+                    if (pageTop >= 700) {
+                        bkgText.style.opacity = 0;
+                    }
+                } else if (cnt == 4 || cnt == 6) {
                     muTopImg.setAttribute('src', 'images/mu-day.jpg');
                 }
                 muTopImg.className = `imgs caption-${cnt}`;
@@ -72,21 +111,15 @@
             pageTop = window.pageYOffset + 450;
             
 
-            // console.log(`cnt ${cnt}`);
-            // console.log(`PTop ${pageTop}`);
-            // console.log(`secTop ${secTops[cnt]}`);
+            console.log(`cnt ${cnt}`);
+            console.log(`PTop ${pageTop}`);
+            console.log(`secTop ${secTops[cnt]}`);
 
             if (pageTop > secTops[cnt]) {
-				cnt++;
-                console.log(`scrolling down ${cnt}`);
-			} else if (cnt > 1 && pageTop < secTops[cnt - 1]) {
-				cnt--;
-                console.log(`scrolling up ${cnt}`);
-			}
-
-            // console.log(cnt);
-
-            // calcSecWidth();
+                cnt++;
+            } else if (cnt > 1 && pageTop < secTops[cnt - 1]) {
+                cnt--;
+            }
 
             if (cnt != prevCnt) {
                 muTopImg.className = `imgs caption-${cnt}`;
@@ -98,24 +131,20 @@
                 if (cnt > prevCnt) {
                     exitDir = 'slide out-focus out-up';
                     enterDir= 'slide on-focus in-up';
-                    // console.log(`scrolling down`);
                 } else {
                     exitDir = 'slide out-focus out-down';
                     enterDir= 'slide on-focus in-down';
-                    // console.log(`scrolling up`);
                 }
-
-                // console.log(`current counter ${cnt}`);
-                // console.log(`previous counter ${prevCnt}`);
                 captions[prevCnt-1].className = exitDir;
                 captions[prevCnt-1].addEventListener('animationend', function() {
                     captions[cnt-1].className = enterDir;
                 });
-                console.log(`cnt1 ${cnt}`);
-                console.log(`precnt1 ${prevCnt}`);
                 prevCnt = cnt;
-                console.log(`cnt2 ${cnt}`);
-                console.log(`precnt2 ${prevCnt}`);
+            }
+
+            if (pageTop < 700 || pageTop == null) {
+                bkgText.style.opacity = 1;
+                theBkg.className = `cls-0`;
             }
         });
 
@@ -128,46 +157,12 @@
             });
         }
 
-        function calcSecWidth() {
-            let secWidth;
-            let top;
-            let bottom;
-            let topLength;
-            let bottomLength;
-            let rectHeight;
-            let height = Math.floor(window.innerHeight);
-            captions.forEach(function(caption) {
-                top = Math.floor(caption.getBoundingClientRect().top);
-                bottom = Math.floor(caption.getBoundingClientRect().bottom);
-                rectHeight = bottom - top;
-                topLength = height-top;
-                bottomLength = height-bottom;
-                // console.log(`current top ${top}`);
-                // console.log(`current bottom ${bottom}`);
-                // console.log(`current bottomLength ${bottomLength}`);
-                if (topLength > 0 && topLength < height) {
-                    if (top >= bottomLength){
-                        if (top <= height) {
-                            secWidth = Math.floor(30 + 5 * (((((rectHeight)/2) + top))/(height/2)-1));
-                            caption.style.width = `${secWidth}vw`;
-                            console.log(`current secWith ${secWidth}`);
-                        }
-                    } else {
-                        if (top > 0) {
-                            secWidth = Math.floor(35 - 5 * (((((rectHeight)/2) + top))/(height/2)));
-                            caption.style.width = `${secWidth}vw`;
-                            console.log(`current secWith ${secWidth}`);
-                        } else if (top < 0 && top > -(rectHeight/2)) {
-                            secWidth = Math.floor(35 - 5 * (((((rectHeight)/2) - top))/(height/2)));
-                            caption.style.width = `${secWidth}vw`;
-                            console.log(`current secWith ${secWidth}`);
-                        }
-                    }
-                }
-                
-            });
-        }
-
+        window.addEventListener('resize', function () {
+            clearTimeout(doneResizing);
+            doneResizing = setTimeout(function () {
+                resetPage();
+            }, 500);
+        });
 
 
         function resetPage() {
@@ -175,30 +170,27 @@
 
             captions.forEach(function(caption) {
                 console.log(caption);
-                let temp = caption.getBoundingClientRect().top
                 secTops.push(Math.floor(caption.getBoundingClientRect().top) + window.pageYOffset);
-                console.log(`client bounding ${caption.getBoundingClientRect().top}`);
+                console.log(secTops.length);
             });
     
             const pagePos = window.pageYOffset + window.innerHeight + 100;
             cnt = 0;
             
-            console.log(`pageTop Yoffset +250 is ${pagePos}`);
+            // console.log(`pageTop Yoffset +250 is ${pagePos}`);
 
             secTops.forEach(function(sec) {
-                console.log(`this is ${sec}`);
+                // console.log(`this is ${sec}`);
                 if (pagePos > sec) {
                     cnt++;
                 }
             });
         }
 
-    });
+    }); // finish the load event listner
     
     
     
-
-
     theBuses.forEach(function (eachBus) {
         console.log(eachBus);
         eachBus.addEventListener('mouseover', function(){
